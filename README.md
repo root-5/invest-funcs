76b6152ae8b23e48dff93676f661f76e71cbf2f1
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-02-16 15:18:22 +0900

    円建口座用関数完成

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # invest-funcs
-投資用の関数を Cloudflare Wokers で提供する
+投資に使える簡易的な関数を Cloudflare Wokers で提供させる試み
+所感: 簡単かつ無料であり、導入～ローカル起動までがマネージドとなっているので楽
 
-# 使用コマンド
+# コマンド
+## リポジトリ作成時に実行
 1. `npm create cloudflare@latest -- invest-funcs`
 2. `npm run deploy`
+
+## 
+- デプロイ: `npm run deploy`
+- ローカル起動: `npm run dev`
--- src/index.js
+++ src/index.js
@@ -1,15 +1,148 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		// リクエストパスを取得
+		const url = new URL(request.url);
+		const path = url.pathname;
+		console.log("PATH: " + path);
+
+		// クエリからトークンを取得
+		const token = url.searchParams.get("token");
+
+		// トークンが正しいか確認
+		if (token !== env.API_TOKEN) {
+			return new Response("Unauthorized", { status: 401 });
+		}
+
+		switch (path) {
+			case "/sbiAccountJPY":
+				const sbiAccountJPY = await getSbiAccountJPY(env.SBI_ID, env.SBI_PASSWORD);
+				return new Response(sbiAccountJPY);
+			case "/sbiAccountUSD":
+				const sbiAccountUSD = await getSbiAccountUSD(env.SBI_ID, env.SBI_PASSWORD);
+				return new Response(sbiAccountUSD);
+			default:
+				return new Response("Not Found", { status: 404 });
+		}
 	},
 };
+
+/**
+ * SBI証券にログインし口座（円建）の情報を CSV 形式で返却する
+ * @param {string} id SBI証券のID
+ * @param {string} password SBI証券のパスワード
+ * @returns {string} CSV形式の口座情報
+ */
+async function getSbiAccountJPY(id, password) {
+	const sbiUrl = "https://site3.sbisec.co.jp/ETGate/";
+
+	// ログイン用パラメータを指定
+	const formData = {
+		_PageID: "WPLETlgR001Rlgn20",
+		_ControlID: "WPLETlgR001Control",
+		_ActionID: "login",
+		_ReturnPageInfo:
+			"WPLETacR001Control/DefaultPID/DefaultAID/DSWPLETacR001Control", // 口座管理-口座（円建）-保有証券の画面を指定している
+		user_id: id,
+		user_password: password,
+	};
+
+	// HTML ソースを取得
+	const res = await fetch(sbiUrl,
+		{
+			method: "POST",
+			body: new URLSearchParams(formData),
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"User-Agent": "Chrome/133.0.0.0",
+			},
+		}
+	);
+	const buffer = await res.arrayBuffer();
+	const uint8Array = new Uint8Array(buffer);
+	const portfolioHtml = new TextDecoder('shift-jis').decode(uint8Array);
+
+	// 株式情報を表示している table 要素の取得
+	const stockTableRegex = /<table border="0" cellspacing="1" cellpadding="1" width="400"><tr><td class="mtext" colspan="4"><font color="#336600">(.*)<\/font><\/b><\/td><\/tr><\/table>/g;
+	const stockTableElem = portfolioHtml.match(stockTableRegex);
+
+	// table 要素から各情報を抽出
+	let match;
+	let stockMarginTypes = [];
+	let stockCodes = [];
+	let stockNames = [];
+	let stockShare = [];
+	let stockBuyingPrices = [];
+	let stockNowPrices = [];
+	const stockCodesRegex = /i_stock_sec=(.{1,6})\+&amp;/g;
+	const stockNamesRegex = /PER=1">(.{1,20})<\/a>/g;
+	const stockShareandPricesRegex = /<td class="mtext">(.{1,10})<\/td>/g;
+	for (let i = 0; i < stockTableElem.length; i++) {
+		const marginType = i === 0 ? "現物" : "信用";
+		while ((match = stockCodesRegex.exec(stockTableElem[i])) !== null) {
+			stockMarginTypes.push(marginType);
+			stockCodes.push(match[1]);
+		}
+		while ((match = stockNamesRegex.exec(stockTableElem[i])) !== null) {
+			stockNames.push(match[1]);
+		}
+		let count = 0;
+		while ((match = stockShareandPricesRegex.exec(stockTableElem[i])) !== null) {
+			switch (count % 3) {
+				case 0:
+					stockShare.push(Number(match[1].replace(/,/g, "")));
+					break;
+				case 1:
+					stockBuyingPrices.push(Number(match[1].replace(/,/g, "")));
+					break;
+				case 2:
+					stockNowPrices.push(Number(match[1].replace(/,/g, "")));
+					break;
+			}
+			count++;
+		}
+	}
+
+	// オブジェクトに変換
+	const accountInfo = [];
+	for (let i = 0; i < stockCodes.length; i++) {
+		accountInfo.push({
+			margin: stockMarginTypes[i],
+			code: stockCodes[i],
+			name: stockNames[i],
+			share: stockShare[i],
+			buyingPrice: stockBuyingPrices[i],
+			nowPrice: stockNowPrices[i],
+		});
+	}
+
+	// 買付余力を取得
+	const buyingPowerRegex = /<td width="150" class="mtext" align="right"><div class="margin">(.{1,10})&nbsp;/;
+	const buyingPowerMatch = portfolioHtml.match(buyingPowerRegex)[1];
+	const buyingPower = buyingPowerMatch.replace(/,/g, "");
+	accountInfo.push(
+		{
+			margin: "-",
+			code: "-",
+			name: "買付余力",
+			share: "-",
+			buyingPrice: "-",
+			nowPrice: buyingPower,
+		}
+	)
+
+	// csv 形式で返す
+	const csv = accountInfo.map((info) => {
+		return `${info.margin},${info.code},${info.name},${info.share},${info.buyingPrice},${info.nowPrice}`;
+	}).join("\n");
+
+	return csv;
+}
+
+/**
+ * SBI証券にログインし口座（外貨建）の情報を CSV 形式で返却する
+ * @param {string} id SBI証券のID
+ * @param {string} password SBI証券のパスワード
+ * @returns {string} CSV形式の口座情報
+ */
+async function getSbiAccountUSD(id, password) {
+	}
